34f61649b464fcfa131fd98301b57c67cc69a16f
Neharika Rout <neharikarout01@gmail.com>
2024-07-26 11:05:15 +0530

    build for frontend

--- backend/server.js
+++ backend/server.js
@@ -26,10 +26,10 @@ app.use("/api/message", messageRoutes);
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/public")));
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "public", "index.html"))
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
